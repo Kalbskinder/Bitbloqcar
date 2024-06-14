eec6506ee3aa322f22958a7e59b0dc1625ca0ed9
Kalbskinder <141642840+Kalbskinder@users.noreply.github.com>
2024-06-14 14:30:08 +0200

    Update main.ino
    Updated the Mini Servo to be a Contoinous Servo so the steering works in both directions

--- main/main.ino
+++ main/main.ino
@@ -1,86 +1,90 @@
+
+#include <Servo.h>
 #include <BitbloqSoftwareSerial.h>
 
+Servo ServoL;
+Servo ServoR;
+Servo ServoF;
 bqSoftwareSerial board_bluetooth(0, 1, 19200);
-String char_ = board_bluetooth.readString();
-float stop_ = 0;
 float steering_ = 0;
+float stop_ = 0;
 
 void forward() {
-    if (stop_ == 0) {
-        Servo_L.write(180);
-        Servo_R.write(180);
-    } 
-    else {
-        Servo_L.write(90);
-        Servo_R.write(90);
+    while (stop_ == 0) {
+        ServoL.write(180);
+        ServoR.write(180);
+    }
+    if (stop_ == 1) {
+        ServoL.write(90);
+        ServoR.write(90);
     }
 }
 
 
 void backwards() {
-    if (stop_ == 0) {
-        Servo_L.write(0);
-        Servo_R.write(0);
+    while (stop_ == 0) {
+        ServoL.write(0);
+        ServoR.write(0);
     }
-    else {
-        Servo_L.write(90);
-        Servo_R.write(90);
+    if (stop_ == 1) {
+        ServoL.write(90);
+        ServoR.write(90);
     }
 }
 
 
 void left() {
     if (steering_ == 0) {
-        digitalWrite(Status_LED, LOW);
         steering_ = 1;
-        Servo_Steering.write(-75);
+        ServoF.write(180);
         delay(200);
-        Servo_Steering.write(0);
+        ServoF.write(90);
+        ServoF.write(0);
+        delay(200);
+        ServoF.write(90);
         steering_ = 0;
-    }
-    if (steering_ == 1) {
-        digitalWrite(Status_LED, HIGH);
     }
 }
 
 
 void right() {
     if (steering_ == 0) {
-        digitalWrite(Status_LED, LOW);
         steering_ = 1;
-        Servo_Steering.write(75);
+        ServoF.write(0);
         delay(200);
-        Servo_Steering.write(0);
+        ServoF.write(90);
+        ServoF.write(0);
+        delay(200);
+        ServoF.write(90);
         steering_ = 0;
-    }
-    if (steering_ == 1) {
-        digitalWrite(Status_LED, HIGH);
     }
 }
 
 
 void setup() {
+    ServoL.attach(8);
+    ServoR.attach(9);
+    ServoF.attach(11);
     board_bluetooth.begin(19200);
-    digitalWrite(Status_LED, LOW);
-    Servo_Steering.write(0);
 }
 
 void loop() {
-    if (char_ == "stop") {
-        stop_ = 1;
-    }
-    if (char_ == "forward") {
+    auto char_ = board_bluetooth.readInt();
+    if (char_ == 1) {
+        stop_ = 0;
         forward();
-        stop_ = 0;
     }
-    if (char_ == "backwards") {
+    if (char_ == 2) {
+        stop_ = 0;
         backwards();
-        stop_ = 0;
     }
-    if (char_ == "left") {
+    if (char_ == 3) {
         left();
     }
-    if (char_ == "right") {
+    if (char_ == 4) {
         right();
+    }
+    if (char_ == 5) {
+        stop_ = 1;
     }
 }
